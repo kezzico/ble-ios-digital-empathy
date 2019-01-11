1c77e225648e3a2d8725ac824475e46185366ae8
Lee Irvine <lee@kezzi.co>
2019-01-10 17:09:06 -0800

    iphone x screen support

--- digital-empathy/HomeIndicatorConstraint.swift
+++ digital-empathy/HomeIndicatorConstraint.swift
@@ -1,6 +1,5 @@
 //
 //  HomeIndicatorConstraint.swift
-//  digital-empathy
 //
 //  Created by Lee Irvine on 1/7/19.
 //  Copyright © 2019 kezzi.co. All rights reserved.
@@ -9,5 +8,14 @@
 import UIKit
 
 class HomeIndicatorConstraint: NSLayoutConstraint {
+    private var constantInitialValue: CGFloat = 0.0
+    
+    override func awakeFromNib() {
+        self.constantInitialValue = self.constant
+        let height = Int(UIScreen.main.nativeBounds.height)
 
+        if height >= 2436 || height == 1792 {
+            self.constant = self.constantInitialValue + 34
+        }
+    }
 }
